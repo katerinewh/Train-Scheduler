be072b6b62891399ecd73776a7f71e8f32c7af33
Katerine <katerinehw@att.net>
2018-11-23 16:06:00 -0800

    initial commit

---
+++ assetts/javascript/script.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD6dbzk_ZKubCmu5AZJXiNciUQyxmZ8onM",
+    authDomain: "train-scheduler-d5029.firebaseapp.com",
+    databaseURL: "https://train-scheduler-d5029.firebaseio.com",
+    projectId: "train-scheduler-d5029",
+    storageBucket: "train-scheduler-d5029.appspot.com",
+    messagingSenderId: "874282800873"
+  };
+  firebase.initializeApp(config);
