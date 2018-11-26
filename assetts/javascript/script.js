7f02336dc4bdc1c5660de46960f93ccf71401d33
Katerine <katerinehw@att.net>
2018-11-26 10:02:37 -0800

    update JS

--- assetts/javascript/script.js
+++ assetts/javascript/script.js
@@ -8,3 +8,25 @@ var config = {
     messagingSenderId: "874282800873"
   };
   firebase.initializeApp(config);
+
+  //adding train name, destination, time, frequency
+  $("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+  
+    var train = $("#name-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var time = moment($("#time-input").val().trim(), "MM/DD/YYYY").format("X");
+    var frequency = $("#frequency-input").val().trim();
+
+    database.ref().push({
+      train: trainInput,
+      destination: destinationInput,
+      time: timeInput,
+      frequency: frequencyInput,
+      });
+
+  });
+
+  database.ref().on("child_added", function (childSnapshot) {
+    $("tbody").append("<tr><td>"+childSnapshot.val().name+"</td>"+"<td>"+childSnapshot.val().role+"</td>"+"<td>"+childSnapshot.val().startDate+"</td>"+"<td>"+"placeholder"+"</td>"+"<td>"+childSnapshot.val().monthlyRate+"</td>"+"<td>"+"placeholder"+"</td>"+"</tr>");
+});
