c9c572209e1f59969bfe292b95914ee46d1787bd
Katerine <katerinehw@att.net>
2018-11-26 17:12:56 -0800

    added javascript amd moment coding

--- assetts/javascript/script.js
+++ assetts/javascript/script.js
@@ -1,32 +1,71 @@
 // Initialize Firebase
+// Initialize Firebase
 var config = {
-    apiKey: "AIzaSyD6dbzk_ZKubCmu5AZJXiNciUQyxmZ8onM",
-    authDomain: "train-scheduler-d5029.firebaseapp.com",
-    databaseURL: "https://train-scheduler-d5029.firebaseio.com",
-    projectId: "train-scheduler-d5029",
-    storageBucket: "train-scheduler-d5029.appspot.com",
-    messagingSenderId: "874282800873"
+  apiKey: "AIzaSyD6dbzk_ZKubCmu5AZJXiNciUQyxmZ8onM",
+  authDomain: "train-scheduler-d5029.firebaseapp.com",
+  databaseURL: "https://train-scheduler-d5029.firebaseio.com",
+  projectId: "train-scheduler-d5029",
+  storageBucket: "train-scheduler-d5029.appspot.com",
+  messagingSenderId: "874282800873"
+};
+
+firebase.initializeApp(config);
+var database = firebase.database();
+
+//adding train name, destination, time, frequency
+$(document).on("click","#add-train-btn", function(event) {
+  event.preventDefault();
+
+  var train = $("#name-input").val().trim();
+  var destination = $("#destination-input").val().trim();
+  var time = moment($("#time-input").val().trim(), "HH:mm").format("X");
+  var frequency = $("#frequency-input").val().trim();
+
+  var newTrain = {
+    train: train,
+    destination: destination,
+    time: time,
+    frequency: frequency,
   };
-  firebase.initializeApp(config);
+  // uploads info to DB//
+  database.ref().push(newTrain);
 
-  //adding train name, destination, time, frequency
-  $("#add-train-btn").on("click", function(event) {
-    event.preventDefault();
-  
-    var train = $("#name-input").val().trim();
-    var destination = $("#destination-input").val().trim();
-    var time = moment($("#time-input").val().trim(), "MM/DD/YYYY").format("X");
-    var frequency = $("#frequency-input").val().trim();
+  console.log(newTrain.train);
+  console.log(newTrain.destination);
+  console.log(newTrain.time);
+  console.log(newTrain.frequency);
 
-    database.ref().push({
-      train: trainInput,
-      destination: destinationInput,
-      time: timeInput,
-      frequency: frequencyInput,
-      });
+  alert("New Train Successfully Added");
 
-  });
+  // clear submission
+  $("#name-input").val("");
+  $("#destination-input").val("");
+  $("#time-input").val("");
+  $("#frequency-input").val("");
 
-  database.ref().on("child_added", function (childSnapshot) {
-    $("tbody").append("<tr><td>"+childSnapshot.val().name+"</td>"+"<td>"+childSnapshot.val().role+"</td>"+"<td>"+childSnapshot.val().startDate+"</td>"+"<td>"+"placeholder"+"</td>"+"<td>"+childSnapshot.val().monthlyRate+"</td>"+"<td>"+"placeholder"+"</td>"+"</tr>");
+});
+
+// firebase
+database.ref().on("child_added", function (childSnapshot) {
+  //get current time
+var currentTime= moment();
+  //get train start from the database
+  var trainStart= childSnapshot.val().time;
+      // Pushing the start time back 1 yr to ensure it comes before the current time
+var yearTime= moment(trainStart, "hh:mm").subtract(1, "years")
+    // calculate the difference between trainStart and curTime
+var timeDifference= moment().diff(moment(yearTime, "minutes"));
+        // calculate time apart
+var timeApart= timeDifference % childSnapshot.val().frequency;
+    // minutes until arrival
+    var minutesTillTrain=childSnapshot.val().frequency - timeApart;
+    //adding minutes until the train to the current time and formatting the appearance of the time
+    var nextArrival=moment().add(minutesTillTrain, "m").format("LT");
+
+
+            // add new row to on-screen table
+$("#tbody").append(newRow)
+
+                // Handle the errors
+
 });
