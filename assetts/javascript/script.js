2fb8acba779759e1f6a5a5d255522f528158a65d
Katerine <katerinehw@att.net>
2018-11-27 12:50:58 -0800

    correct moment errors

--- assetts/javascript/script.js
+++ assetts/javascript/script.js
@@ -18,7 +18,7 @@ $(document).on("click", "#add-train-btn", function (event) {
 
   var train = $("#name-input").val().trim();
   var destination = $("#destination-input").val().trim();
-  var time = moment($("#time-input").val().trim(), "HH:mm").format("X");
+  var time = moment($("#time-input").val().trim(), "HH:mm").format("HH:mm");
   var frequency = $("#frequency-input").val().trim();
 
   var newTrain = {
@@ -56,11 +56,11 @@ database.ref().on("child_added", function (childSnapshot) {
   var frequency = childSnapshot.val().frequency;
 
   //get current time
-  var currentTime = moment();
+  var currentTime = moment().format("HH:mm");
   //get train start from the database
   var trainStart = childSnapshot.val().time;
   // Pushing the start time back 1 yr to ensure it comes before the current time
-  var yearTime = moment(trainStart, "hh:mm").subtract(1, "years")
+  var yearTime = moment(trainStart, "HH:mm").subtract(1, "years")
   // calculate the difference between trainStart and curTime
   var timeDifference = moment().diff(moment(yearTime, "minutes"));
   // calculate time apart
@@ -68,7 +68,7 @@ database.ref().on("child_added", function (childSnapshot) {
   // minutes until arrival
   var minutesTillTrain = childSnapshot.val().frequency - timeApart;
   //adding minutes until the train to the current time and formatting the appearance of the time
-  var nextArrival = moment().add(minutesTillTrain, "m").format("LT");
+  var nextArrival = moment().add(minutesTillTrain, "m").format("HH:mm");
   // info
   console.log(currentTime);
   console.log(trainStart);
@@ -83,7 +83,7 @@ database.ref().on("child_added", function (childSnapshot) {
   var newRow = $("<tr>").append(
     $("<td>").text(train),
     $("<td>").text(destination),
-    $("<td>").text(time),
+    $("<td>").text(trainStart),
     $("<td>").text(frequency),
     $("<td>").text(minutesTillTrain),
     $("<td>").text(nextArrival)
